377d04c497915fb13388c56dccc1a1839b716c1a
cadmusofthebes <45128341+cadmusofthebes@users.noreply.github.com>
2021-12-27 15:08:07 -0500

    Changed from xargs to awk to remove whitespace

--- host2ip.sh
+++ host2ip.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Take an input list of hostnames (one per line) and convert
+# them to IP addresses
+
 inputFilename=$1
 outputFilename="ips.txt"
 
@@ -11,6 +14,6 @@ else
     echo "[*] Processing $inputFilename"
     echo "[*] Outputting results to $outputFilename"
     for host in $(cat $inputFilename); do
-        nslookup $host | grep Address | grep -v "#" | awk -F ":" '{print $2}' | xargs >> $outputFilename
+        nslookup $host | grep Address | grep -v "#" | awk -F ":" '{gsub(/ /,""); print $2}' | xargs >> $outputFilename
     done
 fi
